481ffce7b6fc54150e2af300f5a6d6b3d5c3df5a
kevinnadr <kevin19305.ib@gmail.com>
2024-05-21 21:40:21 +0700

    asosiasi

---
+++ Asosiasi.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class dokter;
+class pasien {
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+    void tambahanDokter(dokter*);
+    void cetakDokter();
+};
+
+class dokter {
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \"" << nama "\" ada\n";
+    }
+    
+    void tambahPasien(pasien*);
+    void cetakPasien();
+};
+
+void pasien::cetakDokter() {
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) {
+        cout << a->nama  << "\n";
+    }
+    cout << endl;
+}
+void dokter::tambahPasien(pasien* pPasien) {
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+void dokter::cetkPasien() {
+    cout << "Daftar Pasien dari dokter \"" this->nama << "\":\n";
+    for (auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
+int main() {
+    dokter* varDokter1 = new dokter("dr.Budi");
+    dokter* varDokter2 = new dokter("dr.Tono");
+    pasien* varPasien1 = new dokter("Andi");
+    pasien* varPasien1 = new dokter("Lia");
+
+    varDokter1->tambahpasien(varPasien1);
+    varDokter1->tambahpasien(varPasien2);
+    varDokter2->tambahpasien(varPasien1);
+
+    varDokter1->cetakpasien();
+    varDokter2->cetakpasien();
+    varPasien1->cetakpasien();
+    varPasien2->cetakpasien();
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
+}
